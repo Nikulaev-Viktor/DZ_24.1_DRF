16f165ea5ff34a8627cc2f59899e5b85468baeb8
Viktor Nikulaev <viktor_1611@mail.ru>
2024-10-26 02:13:11 +0300

    DZ_24.1

---
+++ materials/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+from users.models import NULLABLE
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название', help_text='введите название')
+    description = models.TextField(verbose_name='Описание', help_text='введите описание')
+    preview = models.ImageField(upload_to='materials/image', verbose_name='Изображение', **NULLABLE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс', help_text='выберите курс')
+    title = models.CharField(max_length=100, verbose_name='Название', help_text='введите название')
+    description = models.TextField(verbose_name='Описание', help_text='введите описание')
+    preview = models.ImageField(upload_to='materials/image', verbose_name='Изображение', **NULLABLE)
+    link = models.URLField(max_length=200, verbose_name='Видео', help_text='введите ссылку на видео', **NULLABLE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
